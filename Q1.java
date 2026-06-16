de3093b16b1f6f10f922fc2ed54ae4f868534140
kishor <kishorsakthinarayanan@gmail.com>
2026-06-16 22:48:06 +0530

    Q1 23CB025

---
+++ Q1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input seconds: ");
+        int totalSeconds = sc.nextInt();
+        int hours = totalSeconds / 3600;
+        int minutes = (totalSeconds % 3600) / 60;
+        int seconds = totalSeconds % 60;
+        System.out.printf("%02d:%02d:%02d%n", hours, minutes, seconds);
+    }
+}
