a2229000fe961a9b0bda54031511a48c59d95526
kishor <kishorsakthinarayanan@gmail.com>
2026-06-16 22:51:08 +0530

    Q2 23CB025

---
+++ Q2.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Q2 {
+
+    public static boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0)
+                return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+        int number = 2;
+        int sum = 0;
+
+        while (count < 100) {
+            if (isPrime(number)) {
+                sum += number;
+                count++;
+            }
+            number++;
+        }
+
+        System.out.println("Sum of the first 100 prime numbers: " + sum);
+    }
+}
